86c3d672e6113fe9e5b814ee327d327812b2eec2
meamf <meamfoodivoo@gmail.com>
2024-01-29 15:49:29 +0200

    29.01

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ with col2:
 
 st.write("Below you can write anything you want to press or you can just press any key on your keyboard or something like this i dont know i just need  to wrire something")
 
-col3, col4 = st.columns(2)
+col3, empty_col1, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
